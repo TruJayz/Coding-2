25262f70a3d971eabba11d819b7dba0751a94c05
Jay <jalentownsend08@yahoo.com>
2026-02-25 14:37:15 +0000

    classwork

---
+++ Unit 1/PythonOpp.py
@@ -0,0 +1,34 @@
+# Opp = Object orinted programing.
+
+
+
+
+
+    # Create a pet animal class fuction
+    # The class should have 4 properties and 4 methods.
+    # Then create 3 pets objects. Each pet should have unique properties and use at least 1 methods.
+
+class MyClass:
+    def __init__(self):
+        self.x = 0
+
+def Class():
+    p1 = MyClass()
+    print(p1.x)
+
+
+
+    def feedPet(self):
+        print("Time to feed your pet")
+
+    def sleepingPet(self):
+        print("Time for your pet to sleep") 
+
+    def playTime(self):
+        print("Time to play with your pet") 
+
+    def bathroom(self):
+        print("Time for your pet to go to the bathroom")
+
+
+    pet_1 = Petcreator("brown")
