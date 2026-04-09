30e927873a1aa2243aef1e95e7df2720883f3947
Jay <jalentownsend08@yahoo.com>
2026-04-09 13:36:49 +0000

    work

--- apilession.py
+++ apilession.py
@@ -1,9 +1,28 @@
+from urllib import response
+
 import requests
 
 
 
-url = 'https://jsonplaceholder.typicode.com/posts/1'
-response = requests.get(url)
+ # url = 'https://jsonplaceholder.typicode.com/posts'
+# response = requests.get(url)
 
+
+# print(response.json())
+
+
+
+# import requests
+
+ # url = "https://bored-api.appbrewery.com/filter?type=education"
+# response = requests.get(url)
+
+
+ # print(response.json())
+
+0
+
+url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+response = requests.get(url)
 
 print(response.json())
