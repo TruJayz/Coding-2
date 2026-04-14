5767d7eb5f6c3a320d58c1131e7b6c462f0b929c
Jay <jalentownsend08@yahoo.com>
2026-04-14 13:32:38 +0000

    4/14/2026

--- apilession.py
+++ apilession.py
@@ -1,4 +1,4 @@
-from urllib import response
+  # from urllib import response
 
 import requests
 
@@ -20,8 +20,40 @@ import requests
 
  # print(response.json())
 
-0
 
-url = 'https://pokeapi.co/api/v2/pokemon/ditto'
-response = requests.get(url)
-nmhb
+
+ # url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+ #response = requests.get(url)
+
+
+
+
+
+
+
+
+
+
+# 4/14/2026 
+
+
+query = "https://pokeapi.co/api/v2/pokemon/umbreon"
+response = requests.get(query)
+print(response)
+print(response.json())
+
+if response.status_code == 200:
+    data = response.json()
+    
+
+    filtered_data = {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "type": data["types"],
+        "abilities": [ability["ability"]["name"] for ability in data["abilities"]],
+        "sprites":data["sprites"]
+    }
+    print(filtered_data)
+else:
+    print("Failed to retrieve data")
