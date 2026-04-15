3fc20fe9551473922d68d3a84a336e44ae994e7a
Jay <jalentownsend08@yahoo.com>
2026-04-15 13:33:57 +0000

    Studentapi

---
+++ unit 3/Studentapi.py
@@ -0,0 +1,33 @@
+import requests
+
+
+
+
+url = "https://http.cat/status/214"
+response = requests.get(url)
+
+
+print(response.json())
+
+
+import requests
+
+
+url = "https://http.cat/status/202"
+response = requests.get(url)
+
+
+print(response.json())
+
+
+
+
+import requests
+
+
+url = "https://http.cat/status/402"
+response = requests.get(url)
+
+
+
+print(response.json())
--- unit 3/apilession.py
+++ unit 3/apilession.py
@@ -4,8 +4,8 @@ import requests
 
 
 
- # url = 'https://jsonplaceholder.typicode.com/posts'
-# response = requests.get(url)
+url = 'https://jsonplaceholder.typicode.com/posts'
+response = requests.get(url)
 
 
 # print(response.json())
